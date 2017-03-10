6f8d942ff1ef16bb638f3b9c22994d0bb50225d9
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-03-10 15:00:33 +0100

    Created user defined package.

--- FTP Client - Project/src/ftpClientPackage/FTPClient.java
+++ FTP Client - Project/src/ftpClientPackage/FTPClient.java
@@ -1,3 +1,4 @@
+package ftpClientPackage;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
--- FTP Client - Project/src/ftpClientPackage/Main.java
+++ FTP Client - Project/src/ftpClientPackage/Main.java
@@ -1,5 +1,4 @@
-import FTPClient.ConnectionException;
-
+package ftpClientPackage;
 public class Main {
 
 	public static void main(String[] args) {
